caaa985c5c57968e57ffa2fc14f40e00f67529c4
Boburjon2142 <boburjonabduganiyev83@gmail.com>
2025-12-14 18:23:33 +0500

    Prepare project for PythonAnywhere deploy

--- apps/orders/apps.py
+++ apps/orders/apps.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.apps import AppConfig
 
 
@@ -7,3 +8,11 @@ class OrdersConfig(AppConfig):
 
     def ready(self):
         from . import signals  # noqa: F401
+=======
+from django.apps import AppConfig
+
+
+class OrdersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.orders"
+>>>>>>> 93b4415ddc6ddc26ae88b35d34a582fb128c09c3
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,10 @@
-import os 
-import sys 
-from pathlib import Path 
- 
-BASE_DIR = Path(__file__).resolve().parent 
-sys.path.insert(0, str(BASE_DIR)) 
-sys.path.insert(0, str(BASE_DIR / 'backend')) 
- 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings') 
- 
-from django.core.wsgi import get_wsgi_application 
+import sys, os
+
+PROJECT_HOME = '/home/bilimsto/bookstore'
+if PROJECT_HOME not in sys.path:
+    sys.path.insert(0, PROJECT_HOME)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
